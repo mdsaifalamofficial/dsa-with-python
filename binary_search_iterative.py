c6f370fe330d1936e4f15ba1bc02b836dc51c927
Md Saif Alam <mdsaifalamofficial@gmail.com>
2026-01-23 21:40:48 +0530

    Create binary_search_iterative.py
    add iterative binary search

---
+++ binary_search_iterative.py
@@ -0,0 +1,18 @@
+# Binary Search (Iterative)
+
+arr = [10, 20, 30, 40, 50]
+target = 30
+
+low = 0
+high = len(arr) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == target:
+        print("Found at index", mid)
+        break
+    elif arr[mid] < target:
+        low = mid + 1
+    else:
+        high = mid - 1
